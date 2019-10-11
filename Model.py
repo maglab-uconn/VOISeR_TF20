74e3c2ca69688b03523e7d5f212e67bb3c6b15fd
CODEJIN <codejin2@gmail.com>
2019-10-11 12:50:50 -0400

    Initial upload

---
+++ Model.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+from threading import Thread
+import time, os, sys, argparse
+from collections import Counter
+import Moduels
+from Feeder import Feeder
+
+class VOISeR:
+    def __init__(self):
+        pass
+
+    def Model_Generate(self):
+        pass
+
+    @tf.function
+    def Train_Step(self):
+        pass
+
+    @tf.function
+    def Inference_Step(self):
+        pass
+
+    def Restore(self):
+        pass
+
+    def Train(self):
+        pass
+
+    def Inference(self):
+        pass
+
+    def Export_Inference(self):
+        pass
